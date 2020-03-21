2cc7f29cca801b38ff16280dc21d83ec7952f533
谷口未来 <miku.emp417@gmail.com>
2020-03-21 09:33:35 +0900

    コードを書いて作ったサイドバー

--- functions.php
+++ functions.php
@@ -165,3 +165,56 @@ array(
 );
 }
 add_action( 'widgets_init', 'my_widget_init' );
+
+
+
+/**
+* カスタムフィールドを使ってアクセス数を取得する（特定記事のアクセス数確認用）
+*
+* @param integer $id 投稿id.
+* @return void
+*/
+//アクセス数を取得
+function get_post_views( $id = 0 ){
+global $post;
+//引数が渡されなければ投稿IDを見るように設定
+if ( 0 === $id ) {
+$id = $post->ID;
+}
+$count_key = 'view_counter';
+$count = get_post_meta($id, $count_key, true);
+
+if($count === ''){
+delete_post_meta($id, $count_key);
+add_post_meta($id, $count_key, '0');
+}
+return $count;
+}
+
+/**
+* アクセスカウンター
+*
+* @return void
+*/
+function set_post_views() {
+global $post;
+$count = 0;
+$count_key = 'view_counter';
+
+if($post){
+$id = $post->ID;
+$count = get_post_meta($id, $count_key, true);
+}
+
+if($count === ''){
+delete_post_meta($id, $count_key);
+add_post_meta($id, $count_key, '1');
+}elseif( $count > 0 ){
+if(!is_user_logged_in()){ //管理者（自分）の閲覧を除外
+$count++;
+update_post_meta($id, $count_key, $count);
+}
+}
+//$countが0のままの場合（404や該当記事の検索結果が0件の場合）は何もしない。
+}
+add_action( 'template_redirect', 'set_post_views', 10 );
--- searchform.php
+++ searchform.php
@@ -1,9 +1,9 @@
-  <!-- widget -->
-				<div class="widget widget_search">
-					<div class="widget-title">検索</div>
-					<!-- search-form -->
-				    <form role="search" method="get" class="search-form" action="http://miku.local/">
-						<input type="search" class="search-field" value="" placeholder="キーワード" name="s" id="s">
-            <button type="submit" class="search-submit"><i class="fas fa-search"></i></button>
-					</form><!-- /search-form -->
-				</div><!-- /widget -->
+<!-- widget_search -->
+<div class="widget widget_search">
+<div class="widget-title">検索</div>
+<!-- search-form -->
+<form method="get" class="search-form" action="<?php echo home_url('/'); ?>">
+<input type="search" class="search-field" value="" placeholder="キーワード" name="s" id="s">
+<button type="submit" class="search-submit"><i class="fas fa-search"></i></button>
+</form><!-- /search-form -->
+</div><!-- /widget_search -->
